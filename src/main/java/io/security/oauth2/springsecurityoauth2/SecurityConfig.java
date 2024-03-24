b8fb2d04e077e31f036992012c6efaefaf5d57bf
hk0305 <hyounkeun2009@gmail.com>
2024-03-24 16:22:52 +0900

    feat: 스프링 인증 예외처리에 대한 테스트
     - FormLoginConfigurer
     - ExcpetionHandlingConfigurer
     - defaultEntryPointMappings.put
     - HttpbasicConfigurer
     - defaultEntiryPointMappings.put
     - CustomAuthenticationEntryPoint = 우선!
     - ExceptionTranslationFilter

--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -1,28 +1,36 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+//스프링 시큐리티 6.0 부터 @EnableWebSecurity 에는 @Configuration 포함되지 않음.
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().anyRequest().authenticated();
-        http.formLogin();
-//        http.apply(new CustomSecurityConfigurer().setFlag(true));
+//        http.formLogin();
+//        http.httpBasic();
+        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                log.info("Custom EntryPoint!");
+            }
+        });
         return http.build();
     }
 
-    @Bean
-    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated();
-//        http.formLogin();
-//        http.apply(new CustomSecurityConfigurer().setFlag(true));
-        http.httpBasic();
-        return http.build();
-    }
 
 }
